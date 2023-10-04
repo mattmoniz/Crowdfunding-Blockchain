84f851a43ad7765ffe33d75309e36d77edf56296
Matthew Moniz <Matt@Fastforward.sh>
2023-10-03 21:09:14 -0400

    built campaign details component

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -44,26 +44,49 @@ export const StateContextProvider = ({ children }) => {
     const campaigns = await contract.call("getCampaigns");
 
     const parsedCampaigns = campaigns.map((campaign, i) => ({
-          owner: campaign.owner,
-          title: campaign.title,
-          description: campaign.description,
-          target: ethers.utils.formatEther(campaign.target.toString()),
-          deadline: campaign.deadline.toNumber(),
-          amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
-          image: campaign.image,
-          pId: i
-
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i,
     }));
     return parsedCampaigns;
   };
 
   const getUserCampaigns = async () => {
     const allCampaigns = await getCampaigns();
-    const filteredCampaigns = allCampaigns.filter((campaign) =>campaign.owner===address);
-      return filteredCampaigns;
-  }
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+    return filteredCampaigns;
+  };
 
+  const donate = async (pId, amount) => {
+    const data = await contract.call("donateToCampaign", [pId], {
+      value: ethers.utils.parseEther(amount),
+    });
 
+    return data;
+  };
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call("getDonators", [pId]);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for (let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
+      });
+    }
+
+    return parsedDonations;
+  };
 
   return (
     <StateContext.Provider
@@ -73,7 +96,9 @@ export const StateContextProvider = ({ children }) => {
         connect,
         createCampaign: publishCampaign,
         getCampaigns,
-        getUserCampaigns
+        getUserCampaigns,
+        donate,
+        getDonations,
       }}
     >
       {children}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import ReactDom from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
-import { StateContextProvider } from './context';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+import App from "./App";
 
-import './index.css';
-const root = ReactDom.createRoot(document.getElementById('root'));
+import "./index.css";
+const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
-    <ThirdwebProvider activeChain="goerli" desiredChainId={ChainId.Goerli}>
-        <StateContextProvider>
-        <Router>
-            <App />
-        </Router>
-        </StateContextProvider>
-
-    </ThirdwebProvider>
-)
+  <ThirdwebProvider activeChain="goerli" desiredChainId={ChainId.Goerli}>
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
